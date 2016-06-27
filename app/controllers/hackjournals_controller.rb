2fd27197d44376fb14e99eed8d15ad6f0d00030c
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-06-27 15:33:11 +0300

    journal edit/delete

--- app/controllers/hackjournals_controller.rb
+++ app/controllers/hackjournals_controller.rb
@@ -14,18 +14,25 @@ class HackjournalsController < ApplicationController
     end
   end
 
+# <Journal id: 1, journalized_id: 28, journalized_type: "Issue", user_id: 1, notes: "asdadasd", created_on: "2016-06-27 07:22:49", private_notes: false>
+# <JournalDetail id: 1, journal_id: 2, property: "attr", prop_key: "done_ratio", old_value: "1000", value: "40"> 
+
+  def edit_journal
+  end
+
+  def destroy_journal
+  end
+
   private
   def require_admin
     if User.current.admin == true
       return true
     else
       redirect_to :action => :index, :controller => 'projects'
-      flash[:error] = "You are not autharized."
+      flash[:error] = "You are not authorized."
       return false
     end
   end
-
-
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@
 get 'hackjournals', :to => 'hackjournals#index', as: 'hackjournals'
 
 post 'hackjournals/journals', :to => 'hackjournals#show_journals', as: 'show_journals'
+get 'hackjournals/journals/:id/edit', :to => 'hackjournals#edit_journal', as: 'edit_journal'
+delete 'hackjournals/journals/:id/', :to => 'hackjournals#destroy_journal', as: 'destroy_journal'
