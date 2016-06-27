72aae1c1601189306250d15eea47ef770f9861fd
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-06-27 15:01:21 +0300

    admin user check added.

--- app/controllers/hackjournals_controller.rb
+++ app/controllers/hackjournals_controller.rb
@@ -1,11 +1,31 @@
 class HackjournalsController < ApplicationController
   unloadable
+  before_filter :require_admin
 
   def index
   end
 
   def show_journals
-    
+    @issue = Issue.find(params[:hackjournals][:issue_id])
+    if @issue != nil
+      @journals = Journal.find_all_by_journalized_id(@issue.id)
+
+
+    end
   end
+
+  private
+  def require_admin
+    if User.current.admin == true
+      return true
+    else
+      redirect_to :action => :index, :controller => 'projects'
+      flash[:error] = "You are not autharized."
+      return false
+    end
+  end
+
+
+
 
 end
