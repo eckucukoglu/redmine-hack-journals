82e7281a0945d9292ffbf2821fccad6383877504
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-06-28 14:31:25 +0300

    journal edit view ready.

--- app/controllers/hackjournals_controller.rb
+++ app/controllers/hackjournals_controller.rb
@@ -3,24 +3,63 @@ class HackjournalsController < ApplicationController
   before_filter :require_admin
 
   def index
+
   end
 
-  def show_journals
-    @issue = Issue.find(params[:hackjournals][:issue_id])
-    if @issue != nil
-      @journals = Journal.find_all_by_journalized_id(@issue.id)
-
-
+  def show
+    @issueExist = Issue.exists?(id: params[:hackjournals][:issue_id])
+    if @issueExist
+      redirect_to show_journals_path(issue_id: params[:hackjournals][:issue_id])
+    else
+      flash.now[:alert] = "Issue id not exist."
+      render hackjournals_path
     end
   end
 
-# <Journal id: 1, journalized_id: 28, journalized_type: "Issue", user_id: 1, notes: "asdadasd", created_on: "2016-06-27 07:22:49", private_notes: false>
-# <JournalDetail id: 1, journal_id: 2, property: "attr", prop_key: "done_ratio", old_value: "1000", value: "40"> 
+  def show_journals
+    @issue = Issue.find(params[:issue_id])
+    @journals = Journal.find_all_by_journalized_id(@issue.id)
+  end
 
   def edit_journal
+    @issue = Issue.find(params[:issue_id])
+    @journal = Journal.find(params[:id])
+    @jdetails = JournalDetail.find_all_by_journal_id(params[:id])
+  end
+
+  def update_journal
+    @journal = Journal.find(params[:id])
+
+    @year = params[:journal]["created_on(1i)"].to_i
+    @month = params[:journal]["created_on(2i)"].to_i
+    @day = params[:journal]["created_on(3i)"].to_i
+    @hour = params[:journal]["created_on(4i)"].to_i
+    @minute = params[:journal]["created_on(5i)"].to_i
+    @datetime = DateTime.new(@year, @month, @day, @hour, @minute)
+
+    if @journal.update_attributes(:notes => params[:journal][:notes], :created_on => @datetime)
+      flash[:success] = "Journal updated."
+      redirect_to show_journals_path(:issue_id => params[:issue_id])
+    else
+      flash.now[:alert] = "Journal couldn't be updated! Please check the form."
+      render edit_journal_path(:issue_id => params[:issue_id], :id => params[:id])
+    end
+  end
+
+  def update_journal_details
+
   end
 
   def destroy_journal
+    @journal = Journal.find(params[:id])
+
+    if @journal.destroy
+      flash[:success] = "Journal deleted."
+      redirect_to show_journals_path(@journal.journalized_id)
+    else
+      flash.now[:alert] = "Journal couldn't be deleted!"
+      render show_journals_path(@journal.journalized_id)
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-
 get 'hackjournals', :to => 'hackjournals#index', as: 'hackjournals'
+get 'hackjournals/journals', :to => 'hackjournals#show', as: 'show_hackjournals'
+get 'hackjournals/:issue_id/journals', :to => 'hackjournals#show_journals', as: 'show_journals'
 
-post 'hackjournals/journals', :to => 'hackjournals#show_journals', as: 'show_journals'
-get 'hackjournals/journals/:id/edit', :to => 'hackjournals#edit_journal', as: 'edit_journal'
-delete 'hackjournals/journals/:id/', :to => 'hackjournals#destroy_journal', as: 'destroy_journal'
+get 'hackjournals/:issue_id/journals/:id/edit', :to => 'hackjournals#edit_journal', as: 'edit_journal'
+delete 'hackjournals/:issue_id/journals/:id/', :to => 'hackjournals#destroy_journal', as: 'destroy_journal'
+
+put 'hackjournals/:issue_id/journals/:id/', :to => 'hackjournals#update_journal', as: 'update_journal'
+put 'hackjournals/:issue_id/journals/:journal_id/details/:id', :to => 'hackjournals#update_journal_details', as: 'update_journal_details'
