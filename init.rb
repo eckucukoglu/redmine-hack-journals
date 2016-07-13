c6061be3fc5df6fd330b7c0dc5e2e89a977b2a99
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-07-13 12:53:09 +0300

    version tag changed.

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
 Redmine::Plugin.register :hackjournals do
   name 'Hackjournals plugin'
-  url ''
+  url 'https://github.com/eckucukoglu/redmine-hack-journals'
   description 'Hack issue journal details'
   author 'Emre Can Kucukoglu'
   author_url 'http://eckucukoglu.com'
-  version '0.0.1'
-  requires_redmine version_or_higher: '2.5.2'
+  version '0.0.2'
 
   menu :admin_menu, :hackjournals, { :controller => 'hackjournals', :action => 'index' }, :caption => 'HackJournals'
 
